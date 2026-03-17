262bdeb1d493cdeb18e534503eb405e8f8f9f0af
tehilaaizen <t0556742276@gmail.com>
2026-03-17 03:45:29 +0200

    chanege not connect messege

--- src/components/AppBar.jsx
+++
@@ -1,9 +0,0 @@
-// import { Link } from "react-router-dom";
-
-// const AppBar=()=>{
-//     return(<>
-//     <Link to="/Projects" >Task Management</Link>
-//     <Link to="/Login" style={{marginLeft:'20px'}}>Login</Link>
-//     </>)
-// }
-// export default AppBar;
--- src/components/NotConnect.jsx
+++ src/components/NotConnect.jsx
@@ -5,7 +5,7 @@ const NotConnect = () => {
     return (
         <div className="flex flex-column align-items-center justify-content-center p-4">
             <h2 className="m-0">Not Connected</h2>
-            <p className="m-0">Please connect to the internet to access this feature.</p>
+            <p className="m-0">User is not logged in, please log in again.</p>
             <Link to="/"><Button label="Login" icon="pi pi-sign-in" /></Link>
         </div>
     );
